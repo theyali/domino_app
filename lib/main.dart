4071384357792eaf5d74be63fad8c36d4ec2c267
Ali Hajiyev <83508265+theyali@users.noreply.github.com>
2026-08-14 20:55:59 +0400

    Stage 10: check for recoverable game at startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'screens/restaurants_screen.dart';
+import 'screens/app_startup_screen.dart';
 
 void main() {
   runApp(const DominoApp());
@@ -13,7 +13,7 @@ class DominoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RestaurantsScreen(),
+      home: AppStartupScreen(),
     );
   }
 }

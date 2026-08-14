cd5814cb118fea75ca47b1d7c8a5a796be0790e2
Ali Hajiyev <83508265+theyali@users.noreply.github.com>
2026-08-15 03:11:26 +0400

    Add avatar URL to user account model

--- lib/models/user_account.dart
+++ lib/models/user_account.dart
@@ -1,14 +1,18 @@
+import '../config/api_config.dart';
+
 class UserAccount {
   final int id;
   final String username;
   final String email;
   final String firstName;
+  final String? avatarUrl;
 
   const UserAccount({
     required this.id,
     required this.username,
     required this.email,
     required this.firstName,
+    this.avatarUrl,
   });
 
   factory UserAccount.fromJson(Map<String, dynamic> json) {
@@ -17,6 +21,7 @@ class UserAccount {
       username: json['username'] as String? ?? '',
       email: json['email'] as String? ?? '',
       firstName: json['first_name'] as String? ?? '',
+      avatarUrl: ApiConfig.resolveUrl(json['avatar_url'] as String?),
     );
   }
 
